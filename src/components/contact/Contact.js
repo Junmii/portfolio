db82823143d5e0b5215c65ede2fa61dec8e56cf9
Junmii <joys.it.projects@gmail.com>
2023-10-02 10:31:36 +0200

    im an idiot

--- src/components/contact/Contact.js
+++ src/components/contact/Contact.js
@@ -6,13 +6,13 @@ import insta from "../../resources/contact/🦆 icon _instagram_.svg"
 export default function Contact() {
     return(
         <div className="contactContainer">
-            <h2>You can reach me at</h2>
+            <h1>You can reach me at</h1>
             <div className="linkContainer">
                 <a href="https://www.instagram.com/tjaptjoyy/"><img className="icon" src={insta}  alt="Link to my Instagram"/></a>
                 <a href="https://www.linkedin.com/in/joy-krabbe-0350b0200/"><img className="icon" src={linkedIn} alt="Link to my LinkedIn"/></a>
                 <a href="https://github.com/Junmii"><img className="icon" src={github} alt="Link to my GitHub"/></a>
             </div>
-            <h2 className="emailH2">Or send me an e-mail!</h2>
+            <h1 className="emailH2">Or send me an e-mail!</h1>
             <p>joy.krabbe@gmail.com</p>
         </div>
     );
